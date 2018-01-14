0ae2c4e83ab84d302e3587b565400f1067051044
belalcse <belal.gub@gmail.com>
2018-01-14 13:39:14 +0600

    404 page not found page

---
+++ sorry.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Document</title>
+    <style>
+        @import url(https://fonts.googleapis.com/css?family=Righteous);*{margin:0;padding:0}body{font-family:Righteous,cursive;background-color:#f0f0f0}.page_wrapper{width:65%;margin:10% auto;transition:.5s;text-align:center}.page_wrapper h1{font-size:150px;color:#3e3e3e;text-shadow:2px 4px 0 #d6d3d3;transition:.5s;text-transform: uppercase;}.page_wrapper h2{font-size:40px;color:#4a4a4a;margin:10px 0;transition:.5s}.page_wrapper p{font-size:20px;color:#4a4a4a;margin:10px 0}.page_wrapper p a{text-decoration:none;color:#ff4e00}.page_wrapper p a:hover{color:#8b2a00}@media only screen and (min-width:320px) and (max-width:479px){.page_wrapper{width:90%}.page_wrapper h1{font-size:100px}.page_wrapper h2{font-size:25px}}
+    </style>
+</head>
+<body>
+    <div class="page_wrapper">
+        <h1>SORRY</h1>
+        <h2>We Couldn't Find that page</h2>
+        <p>Go Back to <a href="index.php">Home Page</a></p>
+    </div>
+</body>
+</html>
